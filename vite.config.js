0ae4875b16d9871a5375b64447657fdb9f9d7eb1
UnknownShopper <the@unknownshoppers.com>
2025-10-15 19:11:02 -0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mapa-policia-puebla/',
-  server: {
+  base: '/puedron/',
+    server: {
     port: 3000,
     open: true
   },
